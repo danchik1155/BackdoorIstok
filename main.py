12c3f82819d8d2f1fadd154b4b9d1c508fa2e1b5
Danila Urvantsev <urvancev-00@mail.ru>
2021-05-26 12:52:20 +0300

    Istok with with relevants V_2

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from Istok_relevants import Ui_Istok_relevants
 from Favorites import Ui_Favorites
 from Search import Ui_Search
 
+def bypercent_key(summ):
+    return summ[1][0]
 
 def connection():
     params = {
@@ -79,17 +81,21 @@ class Istok(QtWidgets.QMainWindow):
         super(Istok, self).__init__()
         self.wind = Ui_Istok()
         self.wind.setupUi(self)
+        self.page = 1
+        self.len_of_relevants = int(4)
         self.initiation()
 
     def initiation(self):
         self.wind.setupUi(self)
+
         self.wind.Allcomp.clicked.connect(self.open_Allcomp)  # Все компании
         self.wind.Relevcomp.clicked.connect(self.open_Relevcomp)  # Наиболее релевантные
         self.wind.Detail.clicked.connect(self.open_Detail)  # Детальное отслеживание
         self.data = connection()
         self.now = datetime.date.today() - datetime.timedelta(days=1)
         self.yesterday = datetime.date.today() - datetime.timedelta(days=2)
-        self.len_of_relevants = int(4)
+
+
 
         try:
             f = open("favorites.txt", 'r')
@@ -106,9 +112,16 @@ class Istok(QtWidgets.QMainWindow):
         self.dialogAll.Back.clicked.connect(self.backbutton)
 
     def open_Relevcomp(self):  # Наиболее релевантные
-        self.find_relevant(self.len_of_relevants)
+        relevants = self.find_relevant(self.len_of_relevants)
         self.dialogRelev = Ui_Istok_relevants()
         self.dialogRelev.setupUi(self)
+        self.dialogRelev.PageNo.setText(str(self.page))
+        self.dialogRelev.Name1.setText(str(relevants[self.page * 2 - 2][0]))
+        self.dialogRelev.Name2.setText(str(relevants[self.page * 2 - 1][0]))
+        self.dialogRelev.Discript1.setText(str(relevants[self.page * 2 - 2][1]))
+        self.dialogRelev.Discript2.setText(str(relevants[self.page * 2 - 1][1]))
+        self.dialogRelev.right.clicked.connect(self.next_page)
+        self.dialogRelev.left.clicked.connect(self.prev_page)
         self.dialogRelev.Back.clicked.connect(self.backbutton)
 
     def find_relevant(self, n: int):
@@ -129,43 +142,62 @@ class Istok(QtWidgets.QMainWindow):
         for stock_data in api_response['data']:
             symbols.update({stock_data['symbol']: stock_data['name']})
             if i < n:
-                relevants_keys.append(stock_data['name'] + "\n")
+                relevants_keys.append(stock_data['symbol'])
                 i += 1
         for stock_data in api_response2['data']:
             if stock_data != []:
                 if stock_data['symbol'] in symbols.keys():
-                    old_favlist.update({symbols[stock_data['symbol']] + "\n": stock_data['high']})
+                    old_favlist.update({stock_data['symbol']: stock_data})
 
         favlist.update(old_favlist)
 
         for stock_data in api_response3['data']:
             if stock_data != []:
                 if stock_data['symbol'] in symbols.keys():
-                    favlist[symbols[stock_data['symbol']] + "\n"] = favlist[symbols[stock_data['symbol']] + "\n"] - \
-                                                                    stock_data['high']
+                    x = float(favlist[stock_data['symbol']]['high'])
+                    y = float(stock_data['high'])
+                    z = x - y
+                    open_day = float(old_favlist[stock_data['symbol']]['open'])
+                    close_day = float(old_favlist[stock_data['symbol']]['close'])
+                    favlist[stock_data['symbol']] = [round(100 * z / x, 3), round(z, 3), round(open_day - close_day, 3)]
 
         for key in favlist.keys():
-            if favlist[key] == old_favlist[key]:
-                favlist[key] = 0
+            if not isinstance(favlist[key], list):
+                favlist[key] = [0.0, 0.0, 0.0]  # первое - проценты, второе - это в деньгах
 
         for key in favlist.keys():
             if key in relevants_keys:
                 continue
             min_key = self.chek_min(relevants_keys, favlist)
-            if favlist[key] > favlist[min_key]:
+            if favlist[key][0] > favlist[min_key][0]:
                 relevants_keys[relevants_keys.index(min_key)] = key
 
+        relevants = []
+
         for i in relevants_keys:
-            print(i+str(favlist[i]))
+            relevants.append([symbols[i], favlist[i]])
+
+        relevants = sorted(relevants, key=bypercent_key, reverse=True)
+        return relevants
 
     def chek_min(self, relevants_keys, favlist):
-        min_value = favlist[relevants_keys[0]]
-        need_key = favlist[relevants_keys[0]]
+        min_value = favlist[relevants_keys[0]][0]
+        need_key = favlist[relevants_keys[0]][0]
         for i in range(len(relevants_keys)):
-            if favlist[relevants_keys[i]] <= min_value:
+            if favlist[relevants_keys[i]][0] <= min_value:
                 need_key = relevants_keys[i]
-                min_value = favlist[relevants_keys[i]]
+                min_value = favlist[relevants_keys[i]][0]
         return need_key
+
+    def next_page(self):
+        if self.page + 1 <= self.len_of_relevants/2:
+            self.page += 1
+            self.open_Relevcomp()
+
+    def prev_page(self):
+        if self.page - 1 >= 1:
+            self.page -= 1
+            self.open_Relevcomp()
 
     def open_Detail(self):  # Детальное отслеживание
         self.dialogDet = Ui_Favorites()
